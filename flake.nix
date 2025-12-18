2dac4f4d16d0081d53e270a064bd5f8b361cfa77
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-12-18 13:11:36 +0100

    feature: basic frontend setup

--- flake.nix
+++ flake.nix
@@ -7,13 +7,13 @@
       pkgs = import nixpkgs { inherit system; };
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          buf
-          protoc-gen-go
-          protoc-gen-connect-go
-          go
-          grpcui
-        ];
+        packages = with pkgs; [ buf go grpcui nodejs_24 pnpm ];
+        shellHook = ''
+          export COREPACK_ENABLE_DOWNLOADS=1
+          export COREPACK_HOME="$PWD/.corepack"
+          export PATH="$COREPACK_HOME/shims:$PATH"
+          corepack prepare pnpm@latest --activate >/dev/null 2>&1 || true
+        '';
       };
     };
 }
